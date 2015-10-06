37e1b54698cc143f7df807e13792eadbaebd482c
T.J. Mahaffey <tj@inthooz.com>
2015-10-05 20:39:59 -0500

    Initial commit.

---
+++ src/VersoServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tjmahaffey\Verso;
+
+use Illuminate\Support\ServiceProvider;
+
+class VersoServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
