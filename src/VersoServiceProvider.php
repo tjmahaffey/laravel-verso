243813fdb6ed645ebc891b8d3026cc7d89f88792
T.J. Mahaffey <tj@inthooz.com>
2015-10-08 21:23:16 -0500

    Corrected directory structure of package. Revised namespacing.

--- src/VersoServiceProvider.php
+++ src/VersoServiceProvider.php
@@ -3,9 +3,17 @@
 namespace Tjmahaffey\Verso;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
 
 class VersoServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
     /**
      * Perform post-registration booting of services.
      *
@@ -13,7 +21,35 @@ class VersoServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // use this if your package has views
+        $this->loadViewsFrom(realpath(__DIR__.'/resources/views'), 'Verso');
+        
+        // use this if your package has routes
+        $this->setupRoutes($this->app->router);
+        
+        // use this if your package needs a config file
+        // $this->publishes([
+        //         __DIR__.'/config/config.php' => config_path('Verso.php'),
+        // ]);
+        
+        // use the vendor configuration file as fallback
+        // $this->mergeConfigFrom(
+        //     __DIR__.'/config/config.php', 'Verso'
+        // );
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function setupRoutes(Router $router)
+    {
+        $router->group(['namespace' => 'Tjmahaffey\Verso\Http\Controllers'], function($router)
+        {
+            require __DIR__.'/Http/routes.php';
+        });
     }
 
     /**
@@ -23,6 +59,18 @@ class VersoServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerVerso();
+        
+        // use this if your package has a config file
+        // config([
+        //         'config/Verso.php',
+        // ]);
+    }
+
+    private function registerVerso()
+    {
+        $this->app->bind('Verso',function($app){
+            return new Verso($app);
+        });
     }
 }
